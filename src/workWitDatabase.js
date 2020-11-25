9c21dafb365d1b1d365bfe622d2347904fbd7553
Vasiliy <vasiliyl@Hexagontech.co.il>
2020-11-25 13:09:36 +0200

    update

--- src/workWitDatabase.js
+++ src/workWitDatabase.js
@@ -8,18 +8,26 @@ export function getDataFromServer() {
 export let promiseStartData = new Promise(function (resolve, reject) {
   //alert('promiseStartData');
   resolve(localStorage.setItem("usersList", JSON.stringify([{ id: 0, name: 'user0', email: 'user0@gmail.com', password: 'password0', group: 'admin' },
-                                                            { id: 1, name: '1', email: '1', password: '1', group: 'user' },
-                                                            { id: 2, name: '2', email: '2', password: '2', group: 'admin' }])));
+  { id: 1, name: '1', email: '1', password: '1', group: 'user' },
+  { id: 2, name: '2', email: '2', password: '2', group: 'admin' }])));
 
 });
 
-export let promiseGetDataFromServer = new Promise(function (resolve, reject) {
+export let getUsers = () => {
+  return new Promise(function (resolve, reject) {
+    resolve(localStorage.setItem("usersList", JSON.stringify([{ id: 0, name: 'user0', email: 'user0@gmail.com', password: 'password0', group: 'admin' },
+    { id: 1, name: '1', email: '1', password: '1', group: 'user' },
+    { id: 2, name: '2', email: '2', password: '2', group: 'admin' }])));
+  })
+}
+
+export let promiseGetDataFromServer = new Promise(function (resolve, reject) {  // use here like getUsers and rename it to getUsers
   //alert('promiseGetDataFromServer');
   resolve(JSON.parse(localStorage.getItem("usersList")));
 
 });
 
-export function promiseUpdateDataOnServer(usersList) {
+export function promiseUpdateDataOnServer(usersList) {  // rename to addUser 
   return new Promise(function (resolve, reject) {
     //alert('promiseUpdateDataOnServer');
     resolve(localStorage.setItem("usersList", JSON.stringify(usersList)));
