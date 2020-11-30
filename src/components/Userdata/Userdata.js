4686d7361dc85d5db3ab23a3b5fae589d99ab149
voloschukk <voloschukk@gmail.com>
2020-12-01 01:19:56 +0200

    Add DragAndDrop

--- src/components/Userdata/Userdata.js
+++ src/components/Userdata/Userdata.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import { getFilesFromServer, changeFileInServer, addFileInServer} from '../../services/workWithDatabase'
+import { getFilesFromServer, changeFileInServer, addFileInServer } from '../../services/workWithDatabase'
 import File from './File'
+import DragAndDrop from './DragAndDrop'
 
 export default class Userdata extends Component {
     constructor(props) {
         super(props);
-        this.state = { files: getFilesFromServer(this.props.user.id) , browseFiles: []};
+        this.state = { files: getFilesFromServer(this.props.user.id), browseFiles: [] };
         this.onSelectFileHandler = this.onSelectFileHandler.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -20,15 +21,24 @@ export default class Userdata extends Component {
         for (var i = 0; i < files.length; i++) {
             browseFiles.push(files[i].name);
         }
-        this.setState({browseFiles: browseFiles});
+        this.setState({ browseFiles: browseFiles });
     }
 
-    handleSubmit(){
+    handleSubmit() {
         const browseFiles = this.state.browseFiles;
         for (var i = 0; i < browseFiles.length; i++) {
             addFileInServer(browseFiles[i], this.props.user.id);
         }
-        this.setState({files: getFilesFromServer(this.props.user.id), browseFiles: []});
+        this.setState({ files: getFilesFromServer(this.props.user.id), browseFiles: [] });
+    }
+
+    handleDrop = (files) => {
+
+        for (var i = 0; i < files.length; i++) {
+            if (!files[i].name) return
+            addFileInServer(files[i].name, this.props.user.id);
+        }
+        this.setState({ files: getFilesFromServer(this.props.user.id), browseFiles: [] });
     }
 
 
@@ -39,36 +49,37 @@ export default class Userdata extends Component {
             <File file={files} renameFile={(file) => this.renameFile(file)} />
         );
 
-        let browseFiles = this.state.browseFiles.reduce(function(accumulator, currentValue) {
+        let browseFiles = this.state.browseFiles.reduce(function (accumulator, currentValue) {
             return currentValue + ", " + accumulator;
-          }, "");
-        browseFiles =  browseFiles.slice(0, -2)
+        }, "");
+        browseFiles = browseFiles.slice(0, -2)
 
         return (
-            <div>
+            <DragAndDrop handleDrop={this.handleDrop}>
+                <div>
 
-                <form class="">
-                    <div class="input-group">
-                        <div class="custom-file">
-                            <input type="file" 
-                            class="custom-file-input" 
-                            multiple="multiple"
-                            onChange={(e) => this.onSelectFileHandler(e.target.files)}
-                            required />
-                            <label class="custom-file-label" >{browseFiles}</label>
+                    <form class="">
+                        <div class="input-group">
+                            <div class="custom-file">
+                                <input type="file"
+                                    class="custom-file-input"
+                                    multiple="multiple"
+                                    onChange={(e) => this.onSelectFileHandler(e.target.files)}
+                                    required />
+                                <label class="custom-file-label" >{browseFiles}</label>
+                            </div>
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-primary" type="button" onClick={this.handleSubmit}>Upload</button>
+                            </div>
                         </div>
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-primary" type="button" onClick={this.handleSubmit}>Upload</button>
-                        </div>
+                    </form>
+
+                    <div class="d-flex align-content-start flex-wrap">
+                        {filesItems}
                     </div>
-                </form>
-
-                <div class="d-flex align-content-start flex-wrap">
-                    {filesItems}
                 </div>
-            </div>
 
-
+            </DragAndDrop>
 
         )
     }
